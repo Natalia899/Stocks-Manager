772b32972ba6f0afedda8fb80f7e2da0f84db74a
MilenaGkm <milenagaranin@gmail.com>
2020-12-09 15:03:19 +0200

    stock info

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,13 +8,16 @@ const FavoriteStock = require("../models/favoriteStocks")
 
 const apiKey = 'AN42IASXL414IVFG'
 
+
+// First get stock info-sends monthly closed price of past year => get stock info by time series\\
 router.get('/stock/:stockName', async (req, res) => {
 	const { stockName } = req.params
 	const stockSymbolSearch = await axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${apiKey}`)
 	const stockSymbol = await stockSymbolSearch.data.bestMatches[0][`1. symbol`]
+	const companyName = await stockSymbolSearch.data.bestMatches[0][`2. name`]
 	const stockApi = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${stockSymbol}&apikey=${apiKey}&limit=12&offset=0`)
 	const stockMonthly = await stockApi.data[`Monthly Time Series`]
-	let months = { name: stockName, info: [] }
+	let months = { Name: companyName, info: [] }
 	for (let i in stockMonthly) {
 		let monthlyClose = { date: i, price: stockMonthly[i][`4. close`] }
 		if (months.info.length < 12) {
@@ -22,6 +25,26 @@ router.get('/stock/:stockName', async (req, res) => {
 		}
 	}
 	res.send(months)
+})
+
+// Basic info of stock \\
+router.get('/stockInfo/:stockName', async (req, res) => {
+	const { stockName } = req.params
+	const stockSymbolSearch = await axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${apiKey}`)
+	const stockSymbol = await stockSymbolSearch.data.bestMatches[0][`1. symbol`]
+	const companyName = await stockSymbolSearch.data.bestMatches[0][`2. name`]
+	const stockApi = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${apiKey}`)
+	const stockInfo = {
+		Name: companyName,
+		Symbol: stockApi.data[`Global Quote`][`01. symbol`],
+		Price: stockApi.data[`Global Quote`][`05. price`],
+		Open: stockApi.data[`Global Quote`][`02. open`],
+		High: stockApi.data[`Global Quote`][`03. high`],
+		Low: stockApi.data[`Global Quote`][`04. low`],
+		Close: stockApi.data[`Global Quote`][`08. previous close`],
+		Volume: stockApi.data[`Global Quote`][`06. volume`]
+	}
+	res.send(stockInfo)
 })
 
 router.get('/login/:userName/:passWord', async (req, res) => {
