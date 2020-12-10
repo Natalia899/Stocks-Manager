7ffce5bdfa6ce2353ea10d0f36688187c88e4ab7
MilenaGkm <milenagaranin@gmail.com>
2020-12-10 10:17:15 +0200

    option by time doesnt work

--- dist/main.js
+++ dist/main.js
@@ -78,9 +78,3 @@ $("#favorites-container").on("click", ".remove", async function () {
 	let data = await manager.removeFavorite(stockName, userId);
 	renderm.renderData({ favoriteStocks: manager.userFavorites }, "stock", "favorites");
 });
-
-$('#types-container').on("click", "#type-button",async () => {
-    const inputVal = $(this).closest("#types-container").find('#time').attr("value")
-    console.log(inputVal);
-    
-})
